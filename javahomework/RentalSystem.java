7769eaaba9fff5730fbff2fd45c1ffd162b82a71
nandininairhm <nandinihm2005@gmail.com>
2025-09-19 08:26:34 +0530

    day8homework

---
+++ javahomework/RentalSystem.java
@@ -0,0 +1,55 @@
+public interface Vehicle {
+    void start();
+    void stop();
+}
+public interface FuelBased {
+    void refuel();
+}
+ class Car implements Vehicle, FuelBased {
+
+  
+    public void start() {
+        System.out.println("Car engine started.");
+    }
+
+   
+    public void stop() {
+        System.out.println("Car engine stopped.");
+    }
+
+    
+    public void refuel() {
+        System.out.println("Car is refueling at the station.");
+    }
+}
+ class ElectricScooter implements Vehicle {
+
+    
+    public void start() {
+        System.out.println("Electric scooter powered on.");
+    }
+
+    
+    public void stop() {
+        System.out.println("Electric scooter powered off.");
+    }
+}
+public class RentalSystem {
+    public static void main(String[] args) {
+        Car car = new Car();
+        ElectricScooter scooter = new ElectricScooter();
+
+       
+        System.out.println("Car behavior:");
+        car.start();
+        car.refuel();
+        car.stop();
+
+        System.out.println();
+
+       
+        System.out.println("Electric Scooter behavior:");
+        scooter.start();
+        scooter.stop();
+    }
+}
